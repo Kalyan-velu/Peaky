955ea246d9249744c948b82c161e26a65890889b
Kalyan Jyoti Borah <kalyanborah456@gmail.com>
2025-12-26 23:31:41 +0530

    (refactor): reorganize main toolbar and enhance theming
    - Refactored the `main-toolbar` component to improve modularity and maintainability by splitting `window-controls` into a separate file.
    - Enhanced theming support by integrating Shoelace styles for light and dark modes.
    - Consolidated CSS variables with Shoelace tokens for consistency.
    - Introduced `iconify-icon` for better icon management.
    - Updated folder selection logic and added real-time theme toggling functionality.
    - Replaced `my-element` with `main-view` and related changes in the frontend structure.
    
    Signed-off-by: Kalyan Jyoti Borah <kalyanborah456@gmail.com>

---
+++ frontend/src/components/main-toolbar/window-controls.ts
@@ -0,0 +1,90 @@
+import { css, html, LitElement } from 'lit';
+import {
+  Quit,
+  WindowIsMaximised,
+  WindowMinimise,
+  WindowToggleMaximise,
+} from '../../../wailsjs/runtime';
+import { customElement, property } from 'lit/decorators.js';
+import 'iconify-icon/dist/iconify-icon';
+
+@customElement('window-controls')
+export class WindowControls extends LitElement {
+  static styles = css`
+    .window-controls {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: fit-content;
+      height: 100%;
+      cursor: pointer;
+      transition: background-color 0.2s;
+      column-gap: 0;
+
+      button {
+        all: unset;
+        display: flex;
+        align-items: stretch;
+        border: none;
+        padding: 0.4em;
+
+        &:hover {
+          background-color: var(--hover-bg);
+        }
+
+        iconify-icon {
+          display: inline;
+          font-size: 0.8em;
+          vertical-align: middle;
+        }
+        &[data-maximized='true'] #fullscreen {
+          display: none;
+        }
+        &[data-maximized='false'] #fullscreen-exit {
+          display: none;
+        }
+      }
+    }
+  `;
+
+  @property({ type: Boolean })
+  maximized = false;
+
+  constructor() {
+    super();
+    void this._checkWindowIsMaximized();
+    window.addEventListener('resize', async () => await this._checkWindowIsMaximized());
+  }
+
+  private async _checkWindowIsMaximized() {
+    const isMaximized = await WindowIsMaximised();
+    this.maximized = isMaximized;
+    return isMaximized;
+  }
+
+  render() {
+    return html`
+      <div class="window-controls">
+        <button @click="${WindowMinimise}" title="Minimize">
+          <iconify-icon icon="mdi:minus"></iconify-icon>
+        </button>
+        <button
+          @click="${WindowToggleMaximise}"
+          data-maximized="${this.maximized}"
+          title="Maximize">
+          <iconify-icon id="fullscreen" icon="codicon:screen-full"></iconify-icon>
+          <iconify-icon id="fullscreen-exit" icon="ci:copy"></iconify-icon>
+        </button>
+        <button class="close" @click="${Quit}" title="Close">
+          <iconify-icon icon="maki:cross"></iconify-icon>
+        </button>
+      </div>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'window-controls': WindowControls;
+  }
+}
--- frontend/types/components/main-toolbar.d.ts
+++ frontend/types/components/main-toolbar.d.ts
@@ -1,8 +1,14 @@
 import { LitElement } from 'lit';
+import 'iconify-icon/dist/iconify-icon';
+import './main-toolbar/window-controls';
 export declare class MainToolbar extends LitElement {
     static styles: import("lit").CSSResult;
     projectTitle: string;
+    isMaximized: boolean;
+    constructor();
     private _openFolder;
+    private _toggleTheme;
+    private _isMaximized;
     render(): import("lit-html").TemplateResult<1>;
 }
 declare global {
